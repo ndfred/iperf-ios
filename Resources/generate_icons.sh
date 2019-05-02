b597bb2c2cc8ae29d9d076d7956c0b12d9d5f6fb
Frédéric Sagnes <frederic.sagnes@gmail.com>
2019-05-02 21:56:36 +0100

    Add Icons
    Storyboard is all that is missing for #4

---
+++ Resources/generate_icons.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -ex
+
+cd "`$PWD`"
+
+# FFFFFF-1.0.png is from http://www.1x1px.me/FFFFFF-1.0.png
+sips -s format png --padToHeightWidth 120 120 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-120.png
+sips -s format png --padToHeightWidth 152 152 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-152.png
