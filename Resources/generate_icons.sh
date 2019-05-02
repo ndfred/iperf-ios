88ace2798611478edfc6cf3546bff3dda60dbf3b
Frédéric Sagnes <frederic.sagnes@gmail.com>
2019-05-02 22:10:42 +0100

    Add a launch screen storyboard and iPad Pro icon
    Fixes #4

--- Resources/generate_icons.sh
+++ Resources/generate_icons.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -ex
 
-cd "`$PWD`"
+cd "`dirname \"$0\"`"
 
 # FFFFFF-1.0.png is from http://www.1x1px.me/FFFFFF-1.0.png
 sips -s format png --padToHeightWidth 120 120 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-120.png
 sips -s format png --padToHeightWidth 152 152 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-152.png
+sips -s format png --padToHeightWidth 167 167 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-167.png
