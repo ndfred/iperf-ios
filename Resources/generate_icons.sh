ca383e27f3ec36402e04e270a2a9135de696ffa3
Frédéric Sagnes <frederic.sagnes@gmail.com>
2019-05-06 13:47:19 +0100

    Update icons for iOS 9 #7

--- Resources/generate_icons.sh
+++ Resources/generate_icons.sh
@@ -2,8 +2,14 @@
 
 cd "`dirname \"$0\"`"
 
-# FFFFFF-1.0.png is from http://www.1x1px.me/FFFFFF-1.0.png
-sips -s format png --padToHeightWidth 120 120 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-120.png
-sips -s format png --padToHeightWidth 152 152 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-152.png
-sips -s format png --padToHeightWidth 167 167 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-167.png
-sips -s format png --padToHeightWidth 1024 1024 --padColor FFFFFF FFFFFF-1.0.png --out Assets.xcassets/AppIcon.appiconset/Icon-1024.png
+PREFIX="Assets.xcassets/AppIcon.appiconset"
+
+rm -f "$PREFIX/Icon-"*.png
+
+for i in 76 120 152 167 1024
+do
+  # FFFFFF-1.0.png is from http://www.1x1px.me/FFFFFF-1.0.png
+  sips -s format png --padToHeightWidth $i $i --padColor FFFFFF FFFFFF-1.0.png --out "$PREFIX/Icon-$i.png"
+done
+
+mv "$PREFIX/Icon-76.png" "$PREFIX/Icon-77.png"
